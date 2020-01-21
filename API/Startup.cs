c195977a559cbb0c3b4836ce58b1616d71757378
Darren Mart <darren.mart@live.com>
2020-01-20 17:38:00 -0800

    End of Section 4

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,16 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Persistence;
 
 namespace API
@@ -44,6 +38,8 @@ namespace API
                         .WithOrigins("http://localhost:3000");
                 });
             });
+
+            services.AddMediatR(typeof(Application.Activities.List.Handler).Assembly);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
